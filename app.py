d723a1623e5309b75607f82d7421bcdcaf8d58f9
Wendy <wendyvelasquez650@gmail.com>
2025-02-18 20:11:29 -0800

    To-Do List Appilaction - 1

--- app.py
+++ app.py
@@ -1,2 +1,39 @@
-print("apple")
-print("name")
+##To-Do List Application
+
+#To-Do List Application - 1
+
+tasks = []
+
+while True:
+    print("Options:")
+    print("1. Add Task")
+    print("2. Remove Task")
+    print("3. View Tasks")
+    print("4. Exit")
+
+    choice = input("Enter your choice: ")
+
+    if choice == "1":
+        task = input("Enter the task: ")
+        tasks.append(task)
+
+    elif choice == "2":
+        try:
+            task_num = int(input("Enter the number of the task to remove: "))
+            tasks.pop(task_num - 1)
+        except (ValueError, IndexError):
+            print("Invalid task number, try again")
+
+    elif choice == "3":
+        print("To-Do List:")
+        if tasks:
+            for i, task in enumerate(tasks, start=1):
+                print(f"{i}. {task}")
+        else:
+            print("No tasks available")
+
+    elif choice == "4":
+        break
+
+    else:
+        print("Invalid choice, try again")
